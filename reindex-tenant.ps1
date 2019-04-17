f9676e49e5bf853f3b124fb00e4a4b5ee29c97eb
Mikael Svenson <miksvenson@gmail.com>
2019-04-17 09:47:00 +0200

    Base scripts off PnP and support MFA auth

--- reindex-tenant.ps1
+++ reindex-tenant.ps1
@@ -1,126 +1,99 @@
-param([Parameter(Mandatory=$true,ValueFromPipeline=$true)]$url, [Parameter(ValueFromPipeline=$true)][string]$username, [Parameter(ValueFromPipeline=$true)][string]$password, [ValidateSet('skip','on','off')][System.String]$enableAllManagedProperties="skip"  )
+param(
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]$url,
+    [ValidateSet('skip', 'on', 'off')][System.String]$enableAllManagedProperties = "skip",
+    [switch]$includeOneDrive = $false
+)
 # Re-index SPO tenant script, and enable ManagedProperties managed property
 # Author: Mikael Svenson - @mikaelsvenson
-# Blog: http://techmikael.blogspot.com
+# Blog: http://techmikael.com
 
-# Modified by Eric Skaggs on 10/21/2014 - had trouble running this script as it was; functionality has not been changed
-
-function Reset-Webs( $siteUrl )
-{
-	$clientContext = New-Object Microsoft.SharePoint.Client.ClientContext($siteUrl)
-	$clientContext.Credentials = $credentials
-
-	if (!$clientContext.ServerObjectIsNull.Value)
-	{
-		Write-Host "Connected to SharePoint Online site: '$siteUrl'" -ForegroundColor Green
-	}
-
-	$rootWeb = $clientContext.Web
-	processWeb($rootWeb)
+$hasPnP = (Get-Module SharePointPnPPowerShellOnline -ListAvailable).Length
+if ($hasPnP -eq 0) {
+    Write-Host "This script requires PnP PowerShell, trying to install"
+    Find-Module SharePointPnPPowerShellOnline
+    Install-Module SharePointPnPPowerShellOnline
 }
+Import-Module SharePointPnPPowerShellOnline
 
-function processWeb($web)
-{
-	$subWebs = $web.Webs
-	$clientContext.Load($web)
-	$clientContext.Load($web.AllProperties)
-	$clientContext.Load($subWebs)
-	$clientContext.ExecuteQuery()
+function Reset-Webs( $siteUrl ) {
+    Connect-PnPOnline -Url $siteUrl -SPOManagementShell -SkipTenantAdminCheck
+    Write-Host "Processing $siteUrl" -ForegroundColor White
+    $web = Get-PnPWeb
+    if ( $enableAllManagedProperties -ne "skip" ) {
+        Set-AllManagedProperties -web $web -enableAllManagedProps $enableAllManagedProperties
+    }
+    
+    Write-Host "`tSite marked for re-indexing" -ForegroundColor Green
+    Request-PnPReIndexWeb
 
-	Write-Host "Web URL:" $web.Url -ForegroundColor White
-	if( $enableAllManagedProperties -ne "skip" ) {
-		Set-AllManagedProperties -web $web -clientContext $clientContext -enableAllManagedProps $enableAllManagedProperties
-	}
-
-	[int]$version = 0
-	$allProperties = $web.AllProperties
-	if( $allProperties.FieldValues.ContainsKey("vti_searchversion") -eq $true ) {
-		$version = $allProperties["vti_searchversion"]
-	}
-	$version++
-	$allProperties["vti_searchversion"] = $version
-	Write-Host "-- Updated search version: " $version -ForegroundColor Green
-	$web.Update()
-	$clientContext.ExecuteQuery()
-
-	# No need to process subwebs if we only mark site collection for indexing
-	if($enableAllManagedProperties -ne "skip") {
-		foreach ($subWeb in $subWebs)
-		{
-			processWeb($subWeb)
-		}
-	}
-}
-
-function Set-AllManagedProperties( $web, $clientContext, $enableAllManagedProps )
-{
-    $lists = $web.Lists
-    $clientContext.Load($lists)
-    $clientContext.ExecuteQuery()
-
-    foreach ($list in $lists)
-    {
-        Write-Host "--" $list.Title
-
-		if( $list.NoCrawl ) {
-			Write-Host "--  Skipping list due to not being crawled" -ForegroundColor Yellow
-			continue
-		}
-
-		$skip = $false;
-		$eventReceivers = $list.EventReceivers
-		$clientContext.Load($eventReceivers)
-		$clientContext.ExecuteQuery()
-
-		foreach( $eventReceiver in $eventReceivers )
-		{
-			if( $eventReceiver.ReceiverClass -eq "Microsoft.SharePoint.Publishing.CatalogEventReceiver" )
-			{
-				$skip = $true
-				Write-Host "--  Skipping list as it's published as a catalog" -ForegroundColor Yellow
-				break
-			}
-		}
-		if( $skip ) {continue}
-
-		$folder = $list.RootFolder
-		$props = $folder.Properties
-		$clientContext.Load($folder)
-		$clientContext.Load($props)
-		$clientContext.ExecuteQuery()
-
-		if( $enableAllManagedProps -eq "on" ) {
-			Write-Host "--  Enabling all managed properties" -ForegroundColor Green
-			$props["vti_indexedpropertykeys"] = "UAB1AGIAbABpAHMAaABpAG4AZwBDAGEAdABhAGwAbwBnAFMAZQB0AHQAaQBuAGcAcwA=|SQBzAFAAdQBiAGwAaQBzAGgAaQBuAGcAQwBhAHQAYQBsAG8AZwA=|"
-			$props["IsPublishingCatalog"] = "True"
-		}
-		if( $enableAllManagedProps -eq "off" ) {
-			Write-Host "--  Disabling all managed properties" -ForegroundColor Green
-			$props["vti_indexedpropertykeys"] = $null
-			$props["IsPublishingCatalog"] = $null
-		}
-		$folder.Update()
-		$clientContext.ExecuteQuery()
+    if ($enableAllManagedProperties -ne "skip") {
+        $subWebs = Get-PnPSubWebs -Recurse
+        if ($subWebs.Count -gt 0) {
+            foreach ($subWeb in $subWebs) {
+                Reset-Webs($subWeb.Url)
+            }
+        }
     }
 }
 
-Import-Module Microsoft.Online.SharePoint.PowerShell
-# change to the path of your CSOM dlls and add their types
-$csomPath = "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI"
-Add-Type -Path "$csomPath\Microsoft.SharePoint.Client.dll"
-Add-Type -Path "$csomPath\Microsoft.SharePoint.Client.Runtime.dll"
+function Set-AllManagedProperties( $web, $enableAllManagedProps ) {
+    $clientContext = $web.Context
+    $lists = Get-PnPList
 
-if([String]::IsNullOrWhiteSpace($username)) {
-	$username = Read-host "What's your username?"
+    foreach ($list in $lists) {
+        Write-Host "`t$($list.Title)"
+
+        if ( $list.NoCrawl ) {
+            Write-Host "`t`tSkipping list due to not being crawled" -ForegroundColor Yellow
+            continue
+        }
+
+        $skip = $false;
+        $eventReceivers = Get-PnPEventReceiver -List $list
+
+        foreach ( $eventReceiver in $eventReceivers ) {
+            if ( $eventReceiver.ReceiverClass -eq "Microsoft.SharePoint.Publishing.CatalogEventReceiver" ) {
+                $skip = $true
+                Write-Host "`t`tSkipping list as it's published as a catalog" -ForegroundColor Yellow
+                break
+            }
+        }
+        if ( $skip ) { continue }
+
+        $folder = $list.RootFolder
+        $props = $folder.Properties
+        $clientContext.Load($folder)
+        $clientContext.Load($props)
+        $clientContext.ExecuteQuery()
+
+        if ( $enableAllManagedProps -eq "on" ) {
+            Write-Host "`t`tEnabling all managed properties" -ForegroundColor Green
+            $props["vti_indexedpropertykeys"] = "UAB1AGIAbABpAHMAaABpAG4AZwBDAGEAdABhAGwAbwBnAFMAZQB0AHQAaQBuAGcAcwA=|SQBzAFAAdQBiAGwAaQBzAGgAaQBuAGcAQwBhAHQAYQBsAG8AZwA=|"
+            $props["IsPublishingCatalog"] = "True"
+        }
+        if ( $enableAllManagedProps -eq "off" ) {
+            Write-Host "`t`tDisabling all managed properties" -ForegroundColor Green
+            $props["vti_indexedpropertykeys"] = $null
+            $props["IsPublishingCatalog"] = $null
+        }
+        $folder.Update()
+        $clientContext.ExecuteQuery()
+    }
 }
 
-if([String]::IsNullOrWhiteSpace($password)) {
-	$securePassword = Read-host "What's your password?" -AsSecureString
-} else {
-	$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+if ( $url.tolower() -notlike '*-admin*') {
+    Write-Host "This script has to be executed against the admin site of SPO. Eg. https://tenant-admin.sharepoint.com" -ForegroundColor Yellow
+    return
 }
 
-$credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, $securePassword)
-$spoCredentials = New-Object System.Management.Automation.PSCredential($username, $securePassword)
-Connect-SPOService -Url $url -Credential $spoCredentials
-Get-SPOSite -Limit ALL | foreach {Reset-Webs -siteUrl $_.Url }
+Connect-PnPOnline -Url $url -SPOManagementShell -SkipTenantAdminCheck
+Write-Host "Loading site list" -ForegroundColor White
+$sites = Get-PnPTenantSite -IncludeOneDriveSites:$includeOneDrive
+$count = 0
+foreach ($site in $sites) {
+    $count++
+    Write-Host "$count/$($sites.Count)"
+    if ($site.Url -like "*sharepoint.com*") {
+        Reset-Webs -siteUrl $site.Url
+    }
+}
--- reindex-users.ps1
+++ reindex-users.ps1
@@ -1,110 +1,67 @@
-param([Parameter(Mandatory=$true,ValueFromPipeline=$true)]$url, [Parameter(ValueFromPipeline=$true)][string]$username, [Parameter(ValueFromPipeline=$true)][string]$password, [ValidateSet('SPS-Birthday','Department')][System.String]$changeProperty="Department" )
+param(
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]$url,
+    [ValidateSet('SPS-Birthday', 'Department')][System.String]$changeProperty = "Department"
+)
 # Re-index SPO user profiles script
 # Author: Mikael Svenson - @mikaelsvenson
-# Blog: http://techmikael.blogspot.com
+# Blog: http://techmikael.com
 
+$hasPnP = (Get-Module SharePointPnPPowerShellOnline -ListAvailable).Length
+if ($hasPnP -eq 0) {
+    Write-Host "This script requires PnP PowerShell, trying to install"
+    Find-Module SharePointPnPPowerShellOnline
+    Install-Module SharePointPnPPowerShellOnline
+}
+Import-Module SharePointPnPPowerShellOnline
 
-function Reset-UserProfiles( $siteUrl )
-{
-	$clientContext = New-Object Microsoft.SharePoint.Client.ClientContext($siteUrl)
-	$clientContext.Credentials = $credentials
+function Reset-UserProfiles( $siteUrl ) {
+    Write-Host "Retrieving all user profiles" -ForegroundColor Green
+    $profiles = Submit-PnPSearchQuery -Query '-AccountName:spofrm -AccountName:spoapp -AccountName:app@sharepoint -AccountName:spocrawler -AccountName:spocrwl -PreferredName:"Foreign Principal"' -SourceId "b09a7990-05ea-4af9-81ef-edfab16c4e31" -SelectProperties "AccountName", "LastModifiedTime", "PreferredName" `
+        -All -TrimDuplicates:$false -RelevantResults
 
-	if (!$clientContext.ServerObjectIsNull.Value)
-	{
-		Write-Host "Connected to SharePoint Online site: '$siteUrl'" -ForegroundColor Green
-	}
-	$start = 0
-	$rowLimit = 100
-	do
-	{
-		$query = new-object Microsoft.SharePoint.Client.Search.Query.KeywordQuery($clientContext)
-		$query.QueryText="*"
-		$query.SourceId= [Guid]"b09a7990-05ea-4af9-81ef-edfab16c4e31"
-		$query.StartRow=$start
-		$query.RowLimit=$rowLimit
-		$query.SelectProperties.Add("accountname")
-		$query.SelectProperties.Add("write")
-		$query.SelectProperties.Add("crawltime")
-		$query.SelectProperties.Add("PreferredName")
-		$query.TrimDuplicates = $false
-		$executor = new-object Microsoft.SharePoint.Client.Search.Query.SearchExecutor($clientContext)
-		$result = $executor.ExecuteQuery($query)
-		$clientContext.ExecuteQuery()
+    $count = $profiles.Count
+    Write-Host "Iterating $count profiles" -ForegroundColor Green
+    foreach ($p in $profiles) {
+        Write-Host $p.AccountName "Last saved:" $p.LastModifiedTime -ForegroundColor Cyan
+        $props = Get-PnPUserProfileProperty -Account $p.AccountName
 
-		$currentCount = 0
-		if ($result.Value -ne $null)
-		{
-			$currentCount = $result.Value.ResultRows.Length
-			Write-Host "Iterating $currentCount profiles" -ForegroundColor Green
-			$start = ($start + $rowLimit)
-			foreach ($dictionary in $result.Value.ResultRows)
-			{
-				Write-Host $dictionary["accountname"] "Saved:" $dictionary["write"] "Indexed:" $dictionary["crawltime"] -ForegroundColor Cyan
-				$pm = New-Object Microsoft.SharePoint.Client.UserProfiles.PeopleManager($clientContext)
-				$props = $pm.GetPropertiesFor($dictionary["accountname"])
-				$clientContext.Load($props)
-				$clientContext.ExecuteQuery()
+        if ( $changeProperty -eq "SPS-Birthday" ) {
+            $birthday = $props.UserProfileProperties["SPS-Birthday"]
+            if ( $null -eq $birthday) {
+                Write-Host "`tSkipping as user doesn't have the SPS-Birthday field" -ForegroundColor Yellow
+                continue
+            }
 
-				if( $changeProperty -eq "SPS-Birthday" ) {
-					$birthday = $props.UserProfileProperties["SPS-Birthday"]
-					if( $birthday -eq $null) {
-						Write-Host "`tSkipping as user doesn't have the SPS-Birthday field" -ForegroundColor Yellow
-						continue
-					}
-
-					# Force save by setting a random birthday value
-					$pm.SetSingleValueProfileProperty($props.AccountName, "SPS-Birthday",  [DateTime]::Now.ToString("yyyyMMddHHmmss.0Z"))
-					$clientContext.ExecuteQuery()
-
-					if( $birthday -eq "" ) {
-						Write-Host "`tKeeping birthday as not defined" -ForegroundColor Green
-						$pm.SetSingleValueProfileProperty($props.AccountName, "SPS-Birthday",  [String]::Empty)
-					} else {
-						$oldDate = [DateTime]::Parse($birthday)
-						Write-Host "`tRe-setting birthday to" $oldDate -ForegroundColor Green
-						$pm.SetSingleValueProfileProperty($props.AccountName, "SPS-Birthday",  $oldDate)
-					}
-				}
-				if( $changeProperty -eq "Department" ) {
-					$oldDepartment = $props.UserProfileProperties["Department"]
-					if( $oldDepartment -eq $null) {
-						Write-Host "`tSkipping as user doesn't have the Department field" -ForegroundColor Yellow
-						continue
-					}
-					$pm.SetSingleValueProfileProperty($props.AccountName, "Department",  "mAdcOW reindex placeholder")
-					$clientContext.ExecuteQuery()
-					Write-Host "`tRe-setting Department to" $oldDepartment -ForegroundColor Green
-					$pm.SetSingleValueProfileProperty($props.AccountName, "Department",  $oldDepartment)
-				}
-				$clientContext.ExecuteQuery()
-			}
-		}
-	}
-	while ($currentCount -eq $rowLimit)
+            # Force save by setting a random birthday value
+            Set-PnPUserProfileProperty -Account $p.AccountName -PropertyName "SPS-Birthday" -Value [DateTime]::Now.ToString("yyyyMMddHHmmss.0Z")
+            if ( $birthday -eq "" ) {
+                Write-Host "`tKeeping birthday as not defined" -ForegroundColor Green
+                Set-PnPUserProfileProperty -Account $p.AccountName -PropertyName "SPS-Birthday" -Value [String]::Empty
+            }
+            else {
+                $oldDate = [DateTime]::Parse($birthday)
+                Write-Host "`tRe-setting birthday to" $oldDate -ForegroundColor Green
+                Set-PnPUserProfileProperty -Account $p.AccountName -PropertyName "SPS-Birthday" -Value $oldDate
+            }
+        }
+        if ( $changeProperty -eq "Department" ) {
+            $oldDepartment = $props.UserProfileProperties["Department"]
+            if ( $null -eq $oldDepartment) {
+                Write-Host "`tSkipping as user doesn't have the Department field" -ForegroundColor Yellow
+                continue
+            }
+            Set-PnPUserProfileProperty -Account $p.AccountName -PropertyName "Department" -Value "mAdcOW reindex placeholder"
+            Write-Host "`tRe-setting Department to" $oldDepartment -ForegroundColor Green
+            Set-PnPUserProfileProperty -Account $p.AccountName -PropertyName "Department" -Value $oldDepartment
+        }
+        $count--
+        Write-Host "`t$count profiles to go" -ForegroundColor Green
+    }
 }
 
-# change to the path of your CSOM dlls and add their types
-$csomPath = "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI"
-Add-Type -Path "$csomPath\Microsoft.SharePoint.Client.dll"
-Add-Type -Path "$csomPath\Microsoft.SharePoint.Client.Runtime.dll"
-Add-Type -Path "$csomPath\Microsoft.SharePoint.Client.Search.dll"
-Add-Type -Path "$csomPath\Microsoft.SharePoint.Client.UserProfiles.dll"
-
-if([String]::IsNullOrWhiteSpace($username)) {
-	$username = Read-host "What's your username?"
+if ( $url.tolower() -notlike '*-admin*') {
+    Write-Host "This script has to be executed against the admin site of SPO. Eg. https://tenant-admin.sharepoint.com" -ForegroundColor Yellow
+    return
 }
-
-if([String]::IsNullOrWhiteSpace($password)) {
-	$securePassword = Read-host "What's your password?" -AsSecureString
-} else {
-	$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
-}
-
-
-$credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, $securePassword)
-if( $url.tolower() -notlike '*-admin*') {
-	Write-Host "This script has to be executed against the admin site of SPO. Eg. https://tenant-admin.sharepoint.com" -ForegroundColor Yellow
-	return;
-} else {
-	Reset-UserProfiles -siteUrl $url
-}
+Connect-PnPOnline -Url $url -SPOManagementShell -SkipTenantAdminCheck
+Reset-UserProfiles -siteUrl $url
