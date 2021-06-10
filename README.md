5f60973930db6fb1853aa0e591360323103f9fba
Mikael Svenson <misvenso@microsoft.com>
2021-06-10 16:43:12 +0200

    Added optimized script

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # SPO Trigger Re-index scripts by [@mikaelsvenson]
 ## Scripts
 - reindex-tenant.ps1 - Script to mark SharePoint online content to be picked up at the next crawl.
+- reindex-tenant-v2.ps1 - More efficient script to mark SharePoint online content to be picked up at the next crawl.
 
 ## Re-indexing of user profiles
 See [How to trigger re-indexing of user profiles in SharePoint On-line] for an explanation of user profile indexing
@@ -10,10 +11,14 @@ This script will iterate all user profiles in SPO to force a trigger of re-index
 Monitoring has shown that indexing of user profiles happens
 at approximately a **4 hour interval** (per December 10th 2014). I have seen as low as **2h** (night time) and as high as **8h** (daytime).
 
-The script is executes like this:
+Example 1
 
-    .\reindex-users.ps1 -url https://techmikael-admin.sharepoint.com
-0
+    .\reindex-users.ps1 -url https://contoso-admin.sharepoint.com # admin site required
+
 You can also add the switch *-changeProperty* to choose if you want to use *SPS-Birthday* or *Department* (now default) as your change property.
+
+Example 2
+
+    .\reindex-users-v2.ps1 -url https://contoso.sharepoint.com # any non-admin site
 
 [How to trigger re-indexing of user profiles in SharePoint On-line]:http://techmikael.blogspot.com/2014/12/how-to-trigger-re-indexing-of-user.html
---
+++ reindex-users-v2.ps1
@@ -0,0 +1,107 @@
+
+# Re-index SPO user profiles script
+# Author: Mikael Svenson - @mikaelsvenson
+# Blog: http://techmikael.com
+
+<#
+.SYNOPSIS
+Script to trigger re-indexing of all user profiles
+
+.Description
+If you perform search schema mappings after profiles exist you have to update the last modified time on a profile for it to be re-indexed.
+This script ensures all profiles are updated with a new time stamp. Once the import job completes allow 4-24h for profiles to be updated in search.
+
+If used in automation replace Connect-PnPOnline with somethine which works for you.
+
+.Parameter url
+The site you will use to host the import file. Can be any site you have write access to. DO NOT use the admin site.
+
+.Parameter tempPath
+The absolute path on your local disk where we store the import file
+
+.Example 
+.\reindex-users-v2.ps1 -url https://contoso.sharepoint.com -tempPath D:\repos\SPO-Trigger-Reindex
+Specify temp path
+
+.Example 
+.\reindex-users-v2.ps1 -url https://contoso.sharepoint.com
+Use script path as the temp path
+
+#>
+
+param(
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)][string]$url,
+    [Parameter(Mandatory = $false, ValueFromPipeline = $true)][string]$tempPath = $PSScriptRoot
+)
+
+$hasPnP = (Get-Module PnP.PowerShell -ListAvailable).Length
+if ($hasPnP -eq 0) {
+    Write-Output "This script requires PnP PowerShell, trying to install"
+    Find-Module PnP.PowerShell
+    Install-Module PnP.PowerShell
+}
+Import-Module PnP.PowerShell
+
+
+# Replace connection method as needed
+Connect-PnPOnline -Url $url -UseWebLogin
+
+Write-Output "Retrieving all user profiles"
+$profiles = Submit-PnPSearchQuery -Query '-AccountName:spofrm -AccountName:spoapp -AccountName:app@sharepoint -AccountName:spocrawler -AccountName:spocrwl -PreferredName:"Foreign Principal"' `
+    -SourceId "b09a7990-05ea-4af9-81ef-edfab16c4e31" -SelectProperties "aadobjectid", "department", "write" ` -All -TrimDuplicates:$false -RelevantResults
+
+$fragmentTemplate = "{{""IdName"": ""{0}"",""Department"": ""{1}""}}";
+$accountFragments = @();
+
+$profiles |% {
+    $aadId =  $_.aadobjectid + ""
+    $dept = $_.department + ""
+    if(-not [string]::IsNullOrWhiteSpace($aadId) -and $aadId -ne "00000000-0000-0000-0000-000000000000") {
+        $accountFragments += [string]::Format($fragmentTemplate,$aadId,$dept)
+    }
+
+}
+Write-Output "Found $($accountFragments.Count) profiles"
+$json = "{""value"":[" + ($accountFragment -join ',') + "]}"
+
+$propertyMap = @{}
+$propertyMap.Add("Department", "Department")
+
+$filename = "upa-batch-trigger";
+$web = Get-PnPWeb
+$folder = $web.GetFolderByServerRelativeUrl("/");
+
+# Cleanup
+$files = $folder.Files
+$folders = $folder.Folders
+Get-PnPProperty -ClientObject $folder -Property Files,Folders
+
+$files |% {
+    if($_.Name -like "*$filename*") {
+        Write-Output "Remove old import file"
+        $_.DeleteObject()
+    }
+}
+
+$folders |% {
+    if($_.Name -like "*$filename*") {
+        Write-Output "Remove old import status folder"
+        $_.DeleteObject()
+    }
+}
+Invoke-PnPQuery
+# End cleanup
+
+$json > "$tempPath/$filename.txt"
+
+Write-Output "Kicking off import job - Please be patient and allow for 4-24h before profiles are updates in search.`n`nDo NOT re-run because you are impatient!"
+
+
+$job = New-PnPUPABulkImportJob -UserProfilePropertyMapping $propertyMap -IdType CloudId -IdProperty "IdName" -Folder "/" -Path "$tempPath/$filename.txt"
+Remove-Item -Path "$tempPath/$filename.txt"
+
+Write-Output "You can check the status of your job with: Get-PnPUPABulkImportStatus -JobId $($job.JobId)"
+$job
+
+
+
